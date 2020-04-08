e3dc5e44d686330db01c33ce9315ecb80bfe6552
brucekwak <777kwak@gmail.com>
2020-04-08 17:21:26 +0900

    lv_탑 풀이

---
+++ lv2_탑.py
@@ -0,0 +1,30 @@
+﻿# [풀이-1] 기본 풀이
+def solution(heights):
+    
+    answer = [0]  # 첫번째 탑이 보내는 신호 -> 수신X
+    for ind, height in enumerate(heights):
+        if ind == 0:
+            continue
+        check_list = heights[:ind]     
+        find_flag = False
+        for i in range(len(check_list)-1, -1, -1):
+            if check_list[i] > height:
+                answer.append(i+1)
+                find_flag = True
+                break
+        if find_flag == False:
+            answer.append(0)
+    
+    return answer
+
+# [풀이-2] 조금 더 간결한 풀이
+def solution(heights):
+    answer = [0] * len(heights)
+    
+    for i in range(len(heights)):
+        for j in range(i-1, -1, -1):
+            if heights[j] > heights[i]:
+                answer[i] = j+1
+                break
+    
+    return answer
