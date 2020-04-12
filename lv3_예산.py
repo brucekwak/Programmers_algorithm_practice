a147ed66c8c6074f45873d69f129ce3857901578
brucekwak <777kwak@gmail.com>
2020-04-12 17:33:08 +0900

    [binary search] lv3_예산

---
+++ lv3_예산.py
@@ -0,0 +1,25 @@
+﻿def check_possible(budgets, check, M):
+    check_list = []
+    for b in budgets:
+        if b > check:
+            check_list.append(check)
+        else:
+            check_list.append(b)
+    if sum(check_list) > M:
+        return False
+    return True
+
+def solution(budgets, M):
+    left = 0
+    right = max(budgets)
+    
+    answer = 0
+    while left <= right:
+        mid = (left + right) // 2
+        if check_possible(budgets, mid, M) == True:
+            left = mid + 1
+            answer = mid
+        else:
+            right = mid - 1
+    
+    return answer
