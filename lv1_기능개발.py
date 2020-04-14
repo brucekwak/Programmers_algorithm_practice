b150681040435e8d4d5bad5d367e0608c0a7f2ed
brucekwak <777kwak@gmail.com>
2020-04-14 21:05:43 +0900

    lv1_기능개발

---
+++ lv1_기능개발.py
@@ -0,0 +1,14 @@
+﻿import math
+
+def solution(progresses, speeds):    
+    work_days = [math.ceil((100 - prog) / float(speed)) for prog, speed in zip(progresses, speeds)]
+    
+    answer = []
+    front = 0
+    for idx in range(len(work_days)):
+        if work_days[front] < work_days[idx]:
+            answer.append(idx-front)
+            front = idx
+    answer.append(len(work_days)-front)
+    
+    return answer
