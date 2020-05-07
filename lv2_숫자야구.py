f6ce06c1bf29b6d61236fb3a8240045ec2a16b8d
brucekwak <777kwak@gmail.com>
2020-05-07 22:40:49 +0900

    2문제 추가

---
+++ lv2_숫자야구.py
@@ -0,0 +1,26 @@
+﻿# [Reference] https://geonlee.tistory.com/116
+from itertools import permutations
+
+def check_score(candi, baseball):
+    for check_num, s, b in baseball:
+        check_num = [int(x) for x in str(check_num)]
+        strike = 0
+        for i in range(3):
+            if candi[i] == check_num[i]:
+                strike += 1
+        if strike != s:
+            return False
+        ball = len(set(candi) & set(check_num)) - strike
+        if ball != b:
+            return False
+    return True
+
+def solution(baseball):
+    candi_list = list(permutations(list(range(1,10)), 3))
+
+    answer = 0
+    for candi in candi_list:
+        if check_score(candi, baseball) == True:
+            answer += 1
+    
+    return answer
---
+++ lv2_카펫.py
@@ -0,0 +1,8 @@
+﻿def solution(brown, red):
+    total_num = brown + red
+    for num in range(2, int(total_num**0.5)+1):
+        if total_num % num == 0:
+            w = total_num // num
+            h = num
+            if brown == (2*w + 2*h - 4):
+                return [w, h]
