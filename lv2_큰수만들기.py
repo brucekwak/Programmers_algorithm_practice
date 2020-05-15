3cca40fa40a92e1d22c4f494e7745318a0e96543
brucekwak <777kwak@gmail.com>
2020-05-15 22:01:52 +0900

    [탐욕법] 큰수만들기

---
+++ lv2_큰수만들기.py
@@ -0,0 +1,18 @@
+﻿# [Reference] https://gurumee92.tistory.com/162
+
+def solution(number, k):
+    collected = []
+    
+    for (i, num) in enumerate(number):
+        while collected and collected[-1] < num and k > 0:
+            collected.pop()
+            k -= 1
+        
+        if k == 0:
+            collected += number[i:]
+        
+        collected.append(num)
+    
+    collected = collected[:-k] if k > 0 else collected
+    answer = "".join(collected)
+    return answer
